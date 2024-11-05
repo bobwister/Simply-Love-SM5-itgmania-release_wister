79bf47f4694fadbe993048e2bcbe90233acc1262
Wister <bobwister@gmail.com>
2024-11-05 19:02:40 +0100

    + allow ProfileLoad and SelectColor in Pay Mode only in Event Mode (free play)

--- Scripts/SL-Branches.lua
+++ Scripts/SL-Branches.lua
@@ -56,7 +56,7 @@ SelectMusicOrCourse = function()
 end
 
 Branch.AllowScreenSelectProfile = function()
-	if ThemePrefs.Get("AllowScreenSelectProfile") and GAMESTATE:GetCoinMode() ~= "CoinMode_Pay" then
+	if ThemePrefs.Get("AllowScreenSelectProfile") and (GAMESTATE:GetCoinMode() ~= "CoinMode_Pay" or PREFSMAN:GetPreference("EventMode")) then
 		return "ScreenSelectProfile"
 	else
 		return Branch.AllowScreenSelectColor()
@@ -64,7 +64,7 @@ Branch.AllowScreenSelectProfile = function()
 end
 
 Branch.AllowScreenSelectColor = function()
-	if ThemePrefs.Get("AllowScreenSelectColor") and not ThemePrefs.Get("RainbowMode") and GAMESTATE:GetCoinMode() ~= "CoinMode_Pay" then
+	if ThemePrefs.Get("AllowScreenSelectColor") and not ThemePrefs.Get("RainbowMode") and (GAMESTATE:GetCoinMode() ~= "CoinMode_Pay" or PREFSMAN:GetPreference("EventMode")) then
 		return "ScreenSelectColor"
 	else
 		return Branch.AfterScreenSelectColor()
