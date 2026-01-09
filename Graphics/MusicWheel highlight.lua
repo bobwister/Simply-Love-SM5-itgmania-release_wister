6aedef359a399a028067873b764960369669c04d
Wister <bobwister@gmail.com>
2026-01-09 13:36:25 +0100

    Arrow animation for bothe players in Select music

--- Graphics/MusicWheel highlight.lua
+++ Graphics/MusicWheel highlight.lua
@@ -4,7 +4,10 @@ local num_items = THEME:GetMetric("MusicWheel", "NumWheelItems")
 local num_visible_items = num_items - 2
 local item_width = _screen.w / 2.125
 
-return Def.Sprite{
+local af = Def.ActorFrame{}
+
+--animated arrow cursor
+af[#af+1] = Def.Sprite{
 	Texture=THEME:GetPathB("ScreenSelectMusic", "overlay/PerPlayer/arrow.png"),
 	InitCommand=function(self)
 		self:zoom(1.0)
@@ -13,6 +16,19 @@ return Def.Sprite{
 	end,
 }
 
---return Def.Quad{ 
---	InitCommand=function(self) self:horizalign(left):x(WideScale(28,33) - 10):zoomto(item_width + 10,_screen.h/num_visible_items-1) end
---}
+--current speedmod
+for player in ivalues(GAMESTATE:GetHumanPlayers()) do
+	local pn = ToEnumShortString(player)
+	
+	af[#af+1] = LoadFont(ThemePrefs.Get("ThemeFont") .. " Bold")..{
+			InitCommand=function(self)
+				self:diffuse(PlayerColor(player))
+				if (player == PLAYER_1) then self:xy(10, -15) end
+				if (player == PLAYER_2) then self:xy(10, 15) end
+				self:zoom(0.5)
+				self:settext( ("%s%s"):format(SL[pn].ActiveModifiers.SpeedModType, SL[pn].ActiveModifiers.SpeedMod) )
+			end,
+		}
+	end
+		
+return af
